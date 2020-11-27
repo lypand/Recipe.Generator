b2de219f887d6003075f9c4f837b7fe73a026bdd
Andrew Lyp <LYPAND@chrobinson.com>
2020-11-27 01:06:21 -0600

    Adding changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,9 @@ function loadDoc() {
     {
         document.getElementById("h1").innerHTML = data.species.name; 
         document.getElementById("img1").src = data.sprites.front_shiny; 
-        console.log(data); 
+        document.getElementById("description").innerHTML  = data.moves[0].move.name; 
+        document.getElementById("ingredients").innerHTML  = data.sprites.front_shiny; 
+        console.log(data.moves); 
     });
 
 
