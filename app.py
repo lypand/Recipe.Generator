089533a1bbb6d5c159d9bc23f2091982649da096
lypand <63880681+lypand@users.noreply.github.com>
2020-12-02 09:45:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
-from flask import Flask
+from flask import Flask, request
 from recipe_scrapers import scrape_me
 import json
 from flask_cors import CORS
+import random
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
-    Title = scraper.title()
-    Time = scraper.total_time()
-    Quantity = scraper.yields()
-    Ingredients = scraper.ingredients()
-    ret = json.dumps(scraper.schema.data)
-    return (ret)
-    #hELLP
 
-class Recipe():
-    def __init__(self, Title, Time, Quantity, ):
-        self._Title=Title
-        self._Time = str(Time)
-        self._Quantity = str(Quantity)
+def GetRandomRecipe():
+    WebURL = 'https://www.allrecipes.com/recipe/'
+    WebURL = WebURL + str(random.randint(100,19999))
+    scraper = scrape_me(WebURL)
+    return(json.dumps(scraper.schema.data))
+
+
+
+@app.route('/output')
+def hello_world():
+    i = 1
+    ret = GetRandomRecipe()
+    while(ret=='{}'):
+        ret =GetRandomRecipe()
+        i+=1
+    return (str(i) + ret)
+
+@app.route('/input/')
+def Testing():
+    
+    #Whenever url comes in as: localhost:5000/input/?name=INSERTNAME&size=INSERTSIZE
+    #You can add more inputs as well, just seperate in url with '&'
+
+    if 'name' in request.args:
+        name = request.args['name']
+    else:
+        name = 'No Name Specified'
+    if 'size' in request.args:
+        size = request.args['size']
+    else:
+        size = 'No size Specified'
+        
+    return(name+size)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
